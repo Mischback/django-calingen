df383e07fbf82fc614447041e50c92c15b16afd6
Mischback <dev@mischback.de>
2021-11-18 12:46:46 +0100

    Apply black to urls.py

--- calingen/urls.py
+++ calingen/urls.py
@@ -11,6 +11,10 @@ from calingen.views import event
 urlpatterns = [
     path("event/", event.EventListView.as_view(), name="event-list"),
     path("event/<int:event_id>/", event.EventDetailView.as_view(), name="event-detail"),
-    path("event/<int:event_id>/update/", event.EventUpdateView.as_view(), name="event-update"),
+    path(
+        "event/<int:event_id>/update/",
+        event.EventUpdateView.as_view(),
+        name="event-update",
+    ),
     path("event/add/", event.EventCreateView.as_view(), name="event-add"),
 ]
