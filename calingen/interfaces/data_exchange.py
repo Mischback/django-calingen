d3f5b75be0e1f6dfe6f5de6b88d6154bf9ba4001
Mischback <dev@mischback.de>
2021-11-19 22:34:49 +0100

    Add docstring for CalenderEntry

--- calingen/interfaces/data_exchange.py
+++ calingen/interfaces/data_exchange.py
@@ -6,3 +6,19 @@
 from collections import namedtuple
 
 CalenderEntry = namedtuple("CalenderEntry", "title type start")
+"""This data structure is used to pass calender entries around.
+
+Warnings
+--------
+This data structure does not provide any validation of its values / fields.
+
+Following EAFP: the consuming code **must ensure** that the received data is of
+the expected type.
+
+Notes
+-----
+The structure contains an entries ``title``, ``type`` and its ``start`` date and
+time (as :py:obj:`datetime.datetime`).
+
+This is implemented as :py:obj:`collections.namedtuple`.
+"""
