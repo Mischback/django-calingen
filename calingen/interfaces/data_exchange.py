1ad18ae8efb77a7218265785b179e602a044a1ab
Mischback <dev@mischback.de>
2021-11-19 22:00:13 +0100

    Define CalenderEntry

---
+++ calingen/interfaces/data_exchange.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: MIT
+
+"""Provides data exchange formats."""
+
+# Python imports
+from collections import namedtuple
+
+CalenderEntry = namedtuple("CalenderEntry", "title type start")
