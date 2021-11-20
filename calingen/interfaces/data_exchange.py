dcb3c93eb98d1d39f4203d8eca74104104c1211a
Mischback <dev@mischback.de>
2021-11-20 13:39:34 +0100

    Make CalenderEntryList use a set

--- calingen/interfaces/data_exchange.py
+++ calingen/interfaces/data_exchange.py
@@ -25,20 +25,73 @@ This is implemented as :py:obj:`collections.namedtuple`.
 
 
 class CalenderEntryList(object):
-    """A list of calender entries."""
+    """A list of calender entries.
 
-    _entries = []
+    Attributes
+    ----------
+    _entries : set
+        This attribute stores the list of calender entries. It is implemented
+        as a :py:obj:`set`, which makes it mutable and unordered. Primarily a
+        :py:obj:`set` is used to ensure that all included items are **unique**.
+
+    Warnings
+    --------
+    While objects of this class are intended to store / handle instances of
+    :class:`~calingen.interfaces.data_exchange.CalenderEntry`, this is **not**
+    enforced or validated (EAFP).
+    """
+
+    def __init__(self):  # noqa: D107
+        self._entries = set()
 
     def add_entry(self, entry, title=None, category=None, start=None):
-        """Just for linting."""
+        """Add a :class:`~calingen.interfaces.data_exchange.CalenderEntry` to the list.
+
+        Parameters
+        ----------
+        entry : CalenderEntry
+            The entry to be added to this class's list. If set to ``None``, the
+            optional parameters are used to create an instance of
+            :class:`~calingen.interfaces.data_exchange.CalenderEntry` and add
+            that.
+        title: str, optional
+        category: str, optional
+        start: datetime.datetime, optional
+
+        Raises
+        ------
+        Exception
+            If ``entry`` is set to ``None``, ``title``, ``category`` and ``start``
+            are expected to be non ``None`` values (see above for the expected,
+            but not enforced types). Otherwise, the exception is raised.
+
+        Warnings
+        --------
+        There is no validation of the input types!
+        """
         if entry is None:
             if (title is None) or (category is None) or (start is None):
                 raise Exception("Could not add entry!")
 
             entry = CalenderEntry(title, category, start)
 
-        self._entries.append(entry)
+        self._entries.add(entry)
 
     def merge(self, entry_list_instance):
-        """Just for linting."""
-        self._entries += entry_list_instance._entries
+        """Merge two instances of ``CalenderEntryList``.
+
+        Parameters
+        ----------
+        entry_list_instance : CalenderEntryList
+            The instance to be merged into this one.
+
+        Warnings
+        --------
+        There is no validation of the input type!
+
+        Notes
+        -----
+        Providing a ``merge()`` method instead of enabling addition of objects
+        of this type is a design choice and may be subject to change.
+        """
+        self._entries.update(entry_list_instance._entries)
