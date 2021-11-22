e74146e194c5622f192ced474fd15b20a423933a
Mischback <dev@mischback.de>
2021-11-22 21:45:04 +0100

    Fix (project) settings
    Yeah, VisualStudioCode automatically imported something it was not
    supposed to.

--- tests/util/settings_test.py
+++ tests/util/settings_test.py
@@ -6,9 +6,6 @@
 import os
 import sys
 
-# app imports
-from calingen.settings import CALINGEN_EXTERNAL_EVENT_PROVIDER
-
 # Path to the test directory
 TEST_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
