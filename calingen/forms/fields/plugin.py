3563297034b3b805bdf47e8ae9d30c1552a39822
Mischback <dev@mischback.de>
2021-11-28 21:34:48 +0100

    Add missing documentation to forms.fields.plugin

--- calingen/forms/fields/plugin.py
+++ calingen/forms/fields/plugin.py
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: MIT
 
+"""Provide a Django form field to manage plugins."""
+
 # Django imports
 from django.forms.fields import CharField, MultipleChoiceField, MultiValueField
 
@@ -8,10 +10,25 @@ from calingen.forms.widgets import PluginWidget
 
 
 class CalingenListField(CharField):
+    """Misuse a ``CharField`` to handle a list of strings."""
+
     def prepare_value(self, value):
+        """Create a comma seperated list of strings from a Python list.
+
+        Notes
+        -----
+        In Django's normal form rendering workflow, this method is called to
+        process a value for display in a widget. However, for a Django
+        :class:`django.forms.fields.MultiValueField`, the method is not called
+        for `sub fields`.
+
+        However, :meth:`calingen.forms.fields.plugin.PluginField.prepare_value`
+        calls this method.
+        """
         return ", ".join(value)
 
     def to_python(self, value):
+        """Merge a comma-seperated list of strings to a Python list."""
         if not value:
             return []
 
@@ -19,6 +36,7 @@ class CalingenListField(CharField):
 
 
 class PluginField(MultiValueField):
+    """Custom Django field can handle the JSON object related to plugins."""
 
     widget = PluginWidget
 
@@ -33,12 +51,20 @@ class PluginField(MultiValueField):
         self.widget.update_available_plugins(self.choices)
 
     def prepare_value(self, value):
+        """Prepare the value / values for display."""
         prepared_list_field = self.fields[1].prepare_value(value["unavailable"])
         value["unavailable"] = prepared_list_field
 
         return super().prepare_value(value)
 
     def compress(self, data_list):
+        """Compress the values of multiple fields into one object for the ORM layer.
+
+        Notes
+        -----
+        See the corresponding :meth:`calingen.forms.widgets.PluginWidget.decompress`
+        method.
+        """
         if data_list:
             result = {}
             result["active"] = data_list[0]
