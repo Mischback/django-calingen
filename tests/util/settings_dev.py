ea7265efbff036c346ecf7cfff174e405438de76
Mischback <dev@mischback.de>
2021-12-03 15:59:02 +0100

    Move some settings from test to development

--- tests/util/settings_dev.py
+++ tests/util/settings_dev.py
@@ -7,9 +7,41 @@ INSTALLED_APPS += [
     "debug_toolbar",
 ]
 
+# Enable Django's DEBUG mode
+DEBUG = True
+
 MIDDLEWARE += [
+    # add DebugToolbar middleware
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
+
+CALINGEN_EXTERNAL_EVENT_PROVIDER = [
+    "calingen.contrib.holidays.germany",
+]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "dev_f": {
+            "format": "[%(levelname)s] %(name)s:%(lineno)d:%(funcName)s \n\t %(message)s",
+        },
+    },
+    "handlers": {
+        "def_h": {
+            "class": "logging.StreamHandler",
+            "formatter": "dev_f",
+        },
+    },
+    "loggers": {
+        "calingen": {
+            "handlers": ["def_h"],
+            "level": "DEBUG",
+            "propagate": False,
+        }
+    },
+}
+
 
 DEBUG_TOOLBAR_CONFIG = {
     "SHOW_TOOLBAR_CALLBACK": "tests.util.callback_show_debug_toolbar",
