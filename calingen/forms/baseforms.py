93c00667e2d233a16a321d5b5dfccb1aa78cf267
Mischback <dev@mischback.de>
2021-12-10 23:12:37 +0100

    Refactor: Move request-related form code into its own form class

---
+++ calingen/forms/baseforms.py
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: MIT
+
+"""Provide Form implementations with common functionality."""
+
+# Django imports
+from django.forms import Form
+
+
+class RequestEnabledForm(Form):
+    """Form class with the ``request`` object available."""
+
+    def __init__(self, *args, request=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.request = request
--- calingen/forms/tex.py
+++ calingen/forms/tex.py
@@ -3,15 +3,15 @@
 """Provide Form implementations in the context of TeX rendering and compilation."""
 
 # Django imports
-from django.forms import Form
 from django.forms.fields import ChoiceField
 from django.forms.widgets import RadioSelect
 
 # app imports
+from calingen.forms.baseforms import RequestEnabledForm
 from calingen.interfaces.plugin_api import LayoutProvider
 
 
-class TeXLayoutSelectionForm(Form):
+class TeXLayoutSelectionForm(RequestEnabledForm):
     """Select one of the available :class:`~calingen.interfaces.plugin_api.LayoutProvider` instances."""
 
     layout = ChoiceField(
@@ -25,10 +25,6 @@ class TeXLayoutSelectionForm(Form):
     :meth:`LayoutProvider.list_available_plugins() <calingen.interfaces.plugin_api.LayoutProvider.list_available_plugins>`
     and presented using a :class:`~django.forms.widgets.RadioSelect` widget.
     """
-
-    def __init__(self, *args, request=None, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.request = request
 
     def save_selection(self):
         """Save the layout selection in the user's ``Session``."""
