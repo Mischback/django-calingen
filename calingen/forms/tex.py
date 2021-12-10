3cd26bd82e6d8467dc8fa8ac6ccbbed0d113c87c
Mischback <dev@mischback.de>
2021-12-10 20:04:39 +0100

    Add TeXLayoutSelectionForm

---
+++ calingen/forms/tex.py
@@ -0,0 +1,27 @@
+# SPDX-License-Identifier: MIT
+
+"""Provide Form implementations in the context of TeX rendering and compilation."""
+
+# Django imports
+from django.forms import Form
+from django.forms.fields import ChoiceField
+from django.forms.widgets import RadioSelect
+
+# app imports
+from calingen.interfaces.plugin_api import LayoutProvider
+
+
+class TeXLayoutSelectionForm(Form):
+    """Select one of the available :class:`~calingen.interfaces.plugin_api.LayoutProvider` instances."""
+
+    layout = ChoiceField(
+        choices=LayoutProvider.list_available_plugins, widget=RadioSelect
+    )
+    """Presentation of the available :class:`~calingen.interfaces.plugin_api.LayoutProvider`.
+
+    Notes
+    -----
+    The available ``LayoutProvider`` are determined dynamically by calling
+    :meth:`LayoutProvider.list_available_plugins() <calingen.interfaces.plugin_api.LayoutProvider.list_available_plugins>`
+    and presented using a :class:`~django.forms.widgets.RadioSelect` widget.
+    """
