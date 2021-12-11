e2895327f886d1caa516e02ca477f0468dfd6f8e
Mischback <dev@mischback.de>
2021-12-11 08:39:12 +0100

    Add documentation for request enabled forms and views

--- calingen/forms/generic.py
+++ calingen/forms/generic.py
@@ -7,7 +7,15 @@ from django.forms import Form
 
 
 class RequestEnabledForm(Form):
-    """Form class with the ``request`` object available."""
+    """Form class with the ``request`` object available.
+
+    Notes
+    -----
+    This form implementation is intended to be used together with
+    :class:`calingen.views.generic.RequestEnabledFormView`. In fact, an actual
+    form will subclass this form and use a view that is a subclass of
+    :class:`~calingen.views.generic.RequestEnabledFormView`.
+    """
 
     def __init__(self, *args, request=None, **kwargs):
         super().__init__(*args, **kwargs)
--- calingen/views/generic.py
+++ calingen/views/generic.py
@@ -8,7 +8,15 @@ from django.views.generic.edit import FormView
 
 
 class RequestEnabledFormView(FormView):
-    """Basically just a :class:`~django.views.generic.edit.FormView` that makes the ``request`` available on the form."""
+    """Basically just a :class:`~django.views.generic.edit.FormView` that makes the ``request`` available on the form.
+
+    Notes
+    -----
+    This view implementation is intended to be used together with
+    :class:`calingen.forms.generic.RequestEnabledForm`. In fact, an actual view
+    will subclass this view and use a form that is a subclass of
+    :class:`~calingen.forms.generic.RequestEnabledForm`.
+    """
 
     def get_form_kwargs(self, *args, **kwargs):
         """Pass the ``request`` object to the form.
