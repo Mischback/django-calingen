1dc7a5b9bb7ff8927a6781af0676c796b35d61be
Mischback <dev@mischback.de>
2021-12-13 14:39:03 +0100

    Fix coding style issue (redefine input)

--- tests/templatetags/test_calingen_tex.py
+++ tests/templatetags/test_calingen_tex.py
@@ -19,134 +19,134 @@ from ..util.testcases import CalingenTestCase
 class CalingenChecksTest(CalingenTestCase):
     def test_escape_tex_empty(self):
         # Arrange (set up test environment)
-        input = ""
-        output = ""
+        test_input = ""
+        test_output = ""
 
         # Act (actually perform what has to be done)
-        return_value = escape_tex(input)
+        return_value = escape_tex(test_input)
 
         # Assert (verify the results)
-        self.assertEqual(return_value, output)
+        self.assertEqual(return_value, test_output)
 
     def test_escape_tex_ampersand(self):
         # Arrange (set up test environment)
-        input = r"&"
-        output = r"\&"
+        test_input = r"&"
+        test_output = r"\&"
 
         # Act (actually perform what has to be done)
-        return_value = escape_tex(input)
+        return_value = escape_tex(test_input)
 
         # Assert (verify the results)
-        self.assertEqual(return_value, output)
+        self.assertEqual(return_value, test_output)
 
     def test_escape_tex_percent(self):
         # Arrange (set up test environment)
-        input = r"%"
-        output = r"\%"
+        test_input = r"%"
+        test_output = r"\%"
 
         # Act (actually perform what has to be done)
-        return_value = escape_tex(input)
+        return_value = escape_tex(test_input)
 
         # Assert (verify the results)
-        self.assertEqual(return_value, output)
+        self.assertEqual(return_value, test_output)
 
     def test_escape_tex_dollar(self):
         # Arrange (set up test environment)
-        input = r"$"
-        output = r"\$"
+        test_input = r"$"
+        test_output = r"\$"
 
         # Act (actually perform what has to be done)
-        return_value = escape_tex(input)
+        return_value = escape_tex(test_input)
 
         # Assert (verify the results)
-        self.assertEqual(return_value, output)
+        self.assertEqual(return_value, test_output)
 
     def test_escape_tex_hash(self):
         # Arrange (set up test environment)
-        input = r"#"
-        output = r"\#"
+        test_input = r"#"
+        test_output = r"\#"
 
         # Act (actually perform what has to be done)
-        return_value = escape_tex(input)
+        return_value = escape_tex(test_input)
 
         # Assert (verify the results)
-        self.assertEqual(return_value, output)
+        self.assertEqual(return_value, test_output)
 
     def test_escape_tex_underscore(self):
         # Arrange (set up test environment)
-        input = r"_"
-        output = r"\_"
+        test_input = r"_"
+        test_output = r"\_"
 
         # Act (actually perform what has to be done)
-        return_value = escape_tex(input)
+        return_value = escape_tex(test_input)
 
         # Assert (verify the results)
-        self.assertEqual(return_value, output)
+        self.assertEqual(return_value, test_output)
 
     def test_escape_tex_curly_left(self):
         # Arrange (set up test environment)
-        input = r"{"
-        output = r"\{"
+        test_input = r"{"
+        test_output = r"\{"
 
         # Act (actually perform what has to be done)
-        return_value = escape_tex(input)
+        return_value = escape_tex(test_input)
 
         # Assert (verify the results)
-        self.assertEqual(return_value, output)
+        self.assertEqual(return_value, test_output)
 
     def test_escape_tex_curly_right(self):
         # Arrange (set up test environment)
-        input = r"}"
-        output = r"\}"
+        test_input = r"}"
+        test_output = r"\}"
 
         # Act (actually perform what has to be done)
-        return_value = escape_tex(input)
+        return_value = escape_tex(test_input)
 
         # Assert (verify the results)
-        self.assertEqual(return_value, output)
+        self.assertEqual(return_value, test_output)
 
     def test_escape_tex_tilde(self):
         # Arrange (set up test environment)
-        input = r"~"
-        output = r"\texttt{\~{}}"
+        test_input = r"~"
+        test_output = r"\texttt{\~{}}"
 
         # Act (actually perform what has to be done)
-        return_value = escape_tex(input)
+        return_value = escape_tex(test_input)
 
         # Assert (verify the results)
-        self.assertEqual(return_value, output)
+        self.assertEqual(return_value, test_output)
 
     def test_escape_tex_exp(self):
         # Arrange (set up test environment)
-        input = r"^"
-        output = r"\^{}"
+        test_input = r"^"
+        test_output = r"\^{}"
 
         # Act (actually perform what has to be done)
-        return_value = escape_tex(input)
+        return_value = escape_tex(test_input)
 
         # Assert (verify the results)
-        self.assertEqual(return_value, output)
+        self.assertEqual(return_value, test_output)
 
     def test_escape_tex_backslash(self):
         # Arrange (set up test environment)
-        input = r"\ "
-        output = r"$\backslash$ "
+        test_input = r"\ "
+        test_output = r"$\backslash$ "
 
         # Act (actually perform what has to be done)
-        return_value = escape_tex(input)
+        return_value = escape_tex(test_input)
 
         # Assert (verify the results)
-        self.assertEqual(return_value, output)
+        self.assertEqual(return_value, test_output)
 
     def test_escape_tex_full(self):
         # Arrange (set up test environment)
-        input = r"& % $ # _ { } ~ ^ \ \today"
-        output = (
+        test_input = r"& % $ # _ { } ~ ^ \ \today"
+        test_output = (
             r"\& \% \$ \# \_ \{ \} \texttt{\~{}} \^{} $\backslash$ $\backslash$today"
         )
 
         # Act (actually perform what has to be done)
-        return_value = escape_tex(input)
+        return_value = escape_tex(test_input)
 
         # Assert (verify the results)
-        self.assertEqual(return_value, output)
+        self.assertEqual(return_value, test_output)
