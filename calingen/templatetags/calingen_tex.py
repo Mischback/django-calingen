f5c0539c64415221f2aee5d487da85962dd0fced
Mischback <dev@mischback.de>
2021-12-13 14:33:50 +0100

    Add documentation to templatetags.calingen_tex

--- calingen/templatetags/calingen_tex.py
+++ calingen/templatetags/calingen_tex.py
@@ -1,6 +1,13 @@
 # SPDX-License-Identifier: MIT
 
-"""Provide TeX-specific templatetags."""
+"""Provide TeX-specific templatetags.
+
+Notes
+-----
+The required regular expressions to escape strings for TeX are compiled at
+module level and may be re-used. This should boost performance during template
+processing.
+"""
 
 # Python imports
 import logging
@@ -24,7 +31,29 @@ RE_BACKSLASH_FIX = re.compile(r"(\\backslash)")
 @register.filter
 @stringfilter
 def escape_tex(value):
-    """Escape TeX control characters/sequences."""
+    r"""Escape TeX control characters/sequences.
+
+    Parameters
+    ----------
+    value : str
+        The string to be cleaned from TeX control sequences.
+
+    Returns
+    -------
+    str
+        The cleaned / escaped string.
+
+    Notes
+    -----
+    Special characters / sequences to be processed:
+
+    ``& % $ # _ { } ~ ^ \``
+
+    The list of special TeX characters is taken from StackOverflow
+    (see https://tex.stackexchange.com/a/34586). Actually, the same thread has
+    the (Pearl) regular expressions to substitute these special characters
+    aswell (see https://tex.stackexchange.com/a/119383).
+    """
     value = RE_BACKSLASH.sub(r"\\backslash", value)
     value = RE_BATCH.sub(r"\\\1", value)
     value = RE_EXP.sub(r"\^{}", value)
