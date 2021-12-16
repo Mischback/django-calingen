51ef051c04e5ccd053bedbc1fa62f938fbdba043
Mischback <dev@mischback.de>
2021-12-16 14:29:21 +0100

    [Try'n'error] Provide test compilation

--- tests/test_layout_compilation.py
+++ tests/test_layout_compilation.py
@@ -3,15 +3,51 @@
 """Provide tests for calingen.checks."""
 
 # Python imports
+import os
+import subprocess  # nosec: this is on purpose, just for development
+import tempfile
 from unittest import mock, skip  # noqa: F401
 
 # Django imports
-from django.test import override_settings, tag  # noqa: F401
+from django.test import modify_settings, override_settings, tag  # noqa: F401
+
+# app imports
+from calingen.contrib.layouts.simple_event_list.simple_event_list import SimpleEventList
 
 # local imports
 from .util.testcases import CalingenTeXLayoutCompilationTestCase
 
 
 class CalingenContribLayoutCompilationTest(CalingenTeXLayoutCompilationTestCase):
-    def test_foo(self):
-        raise NotImplementedError("To be done!")
+    @modify_settings(
+        INSTALLED_APPS={"append": "calingen.contrib.layouts.simple_event_list"}
+    )
+    def test_simple_event_list_empty(self):
+        # Arrange
+        test_context = {}
+        test_context["target_year"] = 2021
+        test_context["layout_configuration"] = None
+
+        test_filename = "foo.tex"
+        test_output_filename = "foo.pdf"
+
+        rendered_tex = SimpleEventList.render(test_context)
+
+        # Act
+        with tempfile.TemporaryDirectory() as tempdir:
+            with open(os.path.join(tempdir, test_filename), "x", encoding="utf-8") as f:
+                f.write(rendered_tex)
+
+            args = ["lualatex", "-interaction=batchmode", test_filename]
+
+            subprocess.run(
+                args,
+                shell=False,  # nosec: this is on purpose, just for development
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                check=True,
+                cwd=tempdir,
+            )
+
+        # Assert
+        self.assertTrue(os.path.exists(os.path.join(tempdir, test_output_filename)))
