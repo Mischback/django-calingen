82ad3dd9c5539d996f4292248cec22e5ededf2db
Mischback <dev@mischback.de>
2021-12-17 14:04:58 +0100

    Construct a list of days of a calendar

--- calingen/contrib/layouts/year_by_week/year_by_week.py
+++ calingen/contrib/layouts/year_by_week/year_by_week.py
@@ -2,8 +2,14 @@
 
 """:class:`~calingen.interfaces.plugin_api.LayoutProvider` implementation for a year's calender."""
 
+# Python imports
+import logging
+from datetime import date, timedelta
+
 # app imports
 from calingen.interfaces.plugin_api import LayoutProvider
+
+logger = logging.getLogger(__name__)
 
 
 class YearByWeek(LayoutProvider):
@@ -16,4 +22,24 @@ class YearByWeek(LayoutProvider):
 
     @classmethod
     def prepare_context(cls, context):  # noqa: D102
+        # get the target year from context
+        target_year = context.get("target_year")
+
+        # the first day of the calendar is the monday before [YEAR]-01-01
+        first_yearday = date(target_year, 1, 1) - timedelta(
+            days=date(target_year, 1, 1).weekday()
+        )
+        # the last day of the calendar is the sunday after [YEAR]-12-31
+        last_yearday = date(target_year, 12, 31) + timedelta(
+            days=(7 - date(target_year, 12, 31).weekday())
+        )
+
+        # initialize the invariant
+        day = first_yearday
+        while day < last_yearday:
+            logger.debug(day)
+
+            # increment the invariant
+            day = day + timedelta(days=1)
+
         raise NotImplementedError("to be done")
