1d52488df4f7122da7785f7671be89b352b8ec6a
Mischback <dev@mischback.de>
2021-12-17 20:10:30 +0100

    Add tests for YearByWeek layout

--- tests/test_layout_compilation.py
+++ tests/test_layout_compilation.py
@@ -13,6 +13,7 @@ from django.test import modify_settings, override_settings, tag  # noqa: F401
 
 # app imports
 from calingen.contrib.layouts.simple_event_list.simple_event_list import SimpleEventList
+from calingen.contrib.layouts.year_by_week.year_by_week import YearByWeek
 
 # local imports
 from .util.testcases import CalingenTeXLayoutCompilationTestCase
@@ -52,6 +53,42 @@ class CalingenContribLayoutCompilationTest(CalingenTeXLayoutCompilationTestCase)
         test_filename = "simple_event_list_full.tex"
 
         rendered_tex = SimpleEventList.render(test_context)
+
+        # Act
+        return_value = self.write_tex_to_tmp(rendered_tex, test_filename)
+
+        # Assert
+        self.assertTrue(return_value)
+
+    @modify_settings(INSTALLED_APPS={"append": "calingen.contrib.layouts.year_by_week"})
+    def test_year_by_week_empty(self):
+        # Arrange
+        test_context = {}
+        test_context["target_year"] = 2021
+        test_context["layout_configuration"] = None
+
+        test_filename = "year_by_week_empty.tex"
+
+        rendered_tex = YearByWeek.render(test_context)
+
+        # Act
+        return_value = self.write_tex_to_tmp(rendered_tex, test_filename)
+
+        # Assert
+        self.assertTrue(return_value)
+
+    @modify_settings(INSTALLED_APPS={"append": "calingen.contrib.layouts.year_by_week"})
+    def test_year_by_week_with_events(self):
+        # Arrange
+        test_target_year = 2021
+        test_context = {}
+        test_context["entries"] = self.get_entries(test_target_year)
+        test_context["target_year"] = test_target_year
+        test_context["layout_configuration"] = None
+
+        test_filename = "year_by_week_full.tex"
+
+        rendered_tex = YearByWeek.render(test_context)
 
         # Act
         return_value = self.write_tex_to_tmp(rendered_tex, test_filename)
