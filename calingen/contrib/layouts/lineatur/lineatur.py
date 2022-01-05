e48c17aa24033c8b1dddd4c554a9388136816d47
Mischback <dev@mischback.de>
2022-01-05 10:58:36 +0100

    Add paper_size field to LineaturForm

--- calingen/contrib/layouts/lineatur/lineatur.py
+++ calingen/contrib/layouts/lineatur/lineatur.py
@@ -3,12 +3,39 @@
 """:class:`~calingen.interfaces.plugin_api.LayoutProvider` implementation for various ruled paper outputs."""
 
 # Django imports
-from django.forms import CharField
+from django.db.models import TextChoices
+from django.forms import CharField, ChoiceField
 from django.utils.translation import gettext_lazy as _
 
 # app imports
 from calingen.forms.generation import LayoutConfigurationForm
 from calingen.interfaces.plugin_api import LayoutProvider
+
+
+class PaperSize(TextChoices):
+    """Provide choices for the paper sizes."""
+
+    A0 = "paperA0", _("A0 (841mm x 1189mm)")
+    A1 = "paperA1", _("A1 (594mm x 841mm)")
+    A2 = "paperA2", _("A2 (420mm x 594mm)")
+    A3 = "paperA3", _("A3 (297mm x 420mm)")
+    A4 = "paperA4", _("A4 (210mm x 297mm)")
+    A5 = "paperA5", _("A5 (148mm x 210mm)")
+    A6 = "paperA6", _("A6 (105mm x 148mm)")
+    A7 = "paperA7", _("A7 (74mm x 105mm)")
+    B0 = "paperB0", _("B0 (1000mm x 1414mm)")
+    B1 = "paperB1", _("B1 (707mm x 1000mm)")
+    B2 = "paperB2", _("B2 (500mm x 707mm)")
+    B3 = "paperB3", _("B3 (353mm x 500mm)")
+    B4 = "paperB4", _("B4 (250mm x 353mm)")
+    B5 = "paperB5", _("B5 (176mm x 250mm)")
+    B6 = "paperB6", _("B6 (125mm x 176mm)")
+    B7 = "paperB7", _("B7 (88mm x 125mm)")
+    LETTER = "paperLetter", _("Letter (8.5in x 11.0in)")
+    GOV_LETTER = "paperGovLetter", _("Government Letter (8.0in x 10.5in)")
+    LEGAL = "paperLegal", _("Government Legal (8.5in x 14.0in)")
+    LEDGER = "paperLedger", _("Ledger (17.0in x 11.0in)")
+    TABLOID = "paperTabloid", _("Tabloid (11.0in x 17.0in)")
 
 
 class LineaturForm(LayoutConfigurationForm):
@@ -26,6 +53,13 @@ class LineaturForm(LayoutConfigurationForm):
         max_length=150,
         required=False,
         empty_value="",
+    )
+
+    paper_size = ChoiceField(
+        label=_("Paper Size"),
+        help_text=_("The paper size to be used"),
+        choices=PaperSize.choices,
+        required=True,
     )
 
 
