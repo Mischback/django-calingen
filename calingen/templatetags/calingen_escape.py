91c3c0ff1f02c1d8a8ba96d85e7e8505aa727663
Mischback <dev@mischback.de>
2022-01-14 10:01:08 +0100

    Rename some of the pre-defined RegExs
    Fix #41

--- calingen/templatetags/calingen_escape.py
+++ calingen/templatetags/calingen_escape.py
@@ -10,22 +10,20 @@ processing.
 """
 
 # Python imports
-import logging
 import re
 
 # Django imports
 from django import template
 from django.template.defaultfilters import stringfilter
 
-logger = logging.getLogger(__name__)
-
 register = template.Library()
 
 RE_BACKSLASH = re.compile(r"\\")
-RE_BATCH = re.compile(r"([$#&%_{}])")
 RE_EXP = re.compile(r"\^")
 RE_TILDE = re.compile(r"~")
-RE_BACKSLASH_FIX = re.compile(r"(\\backslash)")
+
+RE_TEX_CHARS = re.compile(r"([$#&%_{}])")
+RE_TEX_BACKSLASH_FIX = re.compile(r"(\\backslash)")
 
 
 @register.filter
@@ -54,10 +52,11 @@ def escape_tex(value):
     the (Pearl) regular expressions to substitute these special characters
     aswell (see https://tex.stackexchange.com/a/119383).
     """
+    # The order of substitution *does* matter!
     value = RE_BACKSLASH.sub(r"\\backslash", value)
-    value = RE_BATCH.sub(r"\\\1", value)
+    value = RE_TEX_CHARS.sub(r"\\\1", value)
     value = RE_EXP.sub(r"\^{}", value)
     value = RE_TILDE.sub(r"\\texttt{\~{}}", value)
-    value = RE_BACKSLASH_FIX.sub(r"$\1$", value)
+    value = RE_TEX_BACKSLASH_FIX.sub(r"$\1$", value)
 
     return value
