ddf64ffa71318cabd881c72d6ddd6585c29cdb3f
Mischback <dev@mischback.de>
2022-02-07 10:32:20 +0100

    Provide AppConfig as default_app_config

--- calingen/contrib/providers/german_holidays/__init__.py
+++ calingen/contrib/providers/german_holidays/__init__.py
@@ -2,23 +2,21 @@
 
 """Implementation of :class:`~calingen.interfaces.plugin_api.EventProvider` that provide German holidays, splitted by federate states."""
 
-# local imports
-from .provider import (  # noqa: F401
-    BadenWuerttemberg,
-    Bayern,
-    Berlin,
-    Brandenburg,
-    Bremen,
-    GermanyFederal,
-    Hamburg,
-    Hessen,
-    MecklenburgVorpommern,
-    Niedersachsen,
-    NordrheinWestphalen,
-    RheinlandPfalz,
-    Saarland,
-    Sachsen,
-    SachsenAnhalt,
-    SchleswigHolstein,
-    Thueringen,
+
+default_app_config = (
+    "calingen.contrib.providers.german_holidays.apps.CalingenProviderGermanHolidays"
 )
+"""The path to the app's default configuration class.
+
+Consider this *legacy code*. See
+:djangoapi:`Django's documentation<applications/#configuring-applications>` for
+details.
+
+Notes
+-----
+This application does provide the holidays for Germany, splitted by Federal
+States.
+
+In fact, it is not only *one* implementation of
+:class:`~calingen.interfaces.plugin_api.EventProvider`, but 17.
+"""
